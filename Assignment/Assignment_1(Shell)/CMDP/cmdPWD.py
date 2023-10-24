4eb8459fb49764e287a2ec18ba54fe141f2a5afa
jnthunder <117288245+jnthunder@users.noreply.github.com>
2023-10-23 20:51:48 -0500

    Create cmdPWD.py

---
+++ Assignment/Assignment_1(Shell)/CMDP/cmdPWD.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+
+""" Main funtion: for the implementation of the Head a File command """
+
+##################################### __pwd__ ####################################
+##################################################################################
+
+import os
+
+def pwd(**kwargs):
+
+        print("\n")
+        print(os.getcwd())
+        
