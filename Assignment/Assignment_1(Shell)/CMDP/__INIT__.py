7041b2d7b5300dac1c4318afdad3c3678dc2b8ae
jnthunder <117288245+jnthunder@users.noreply.github.com>
2023-10-23 20:53:23 -0500

    Create __INIT__.py

---
+++ Assignment/Assignment_1(Shell)/CMDP/__INIT__.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3
+
+""" Main listing of Commands and locations for condensed import to the Shell """
+
+################################## __INIT__ ######################################
+##################################################################################
+
+from CMDP.cmdCAT import cat
+from CMDP.cmdCD import cd
+from CMDP.cmdCP import cp
+from CMDP.cmdEXIT import exit
+from CMDP.cmdGREP import grep
+from CMDP.cmdHEAD import head
+from CMDP.cmdHISTORY import createhistory, updateHistory, history
+from CMDP.cmdLESS import less
+# from CMDP.cmdLS import ls, lsA, longListing
+from CMDP.cmdMKDIR import mkDir
+from CMDP.cmdMV import mv
+from CMDP.cmdPWD import pwd
+from CMDP.cmdRM import rm
+from CMDP.cmdTAIL import tail
+from CMDP.cmdWC import wc
