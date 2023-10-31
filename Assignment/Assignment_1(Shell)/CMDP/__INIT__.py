c9976eea2dc6baf269525397a6b4641c5c142449
jnthunder <117288245+jnthunder@users.noreply.github.com>
2023-10-31 13:09:21 -0500

    Update __INIT__.py

--- Assignment/Assignment_1(Shell)/CMDP/__INIT__.py
+++ Assignment/Assignment_1(Shell)/CMDP/__INIT__.py
@@ -7,16 +7,17 @@
 
 from CMDP.cmdCAT import cat
 from CMDP.cmdCD import cd
+from CMDP.cmdCHMOD import chMod
 from CMDP.cmdCP import cp
 from CMDP.cmdEXIT import exit
 from CMDP.cmdGREP import grep
 from CMDP.cmdHEAD import head
 from CMDP.cmdHISTORY import createhistory, updateHistory, history
 from CMDP.cmdLESS import less
-# from CMDP.cmdLS import ls, lsA, longListing
+from CMDP.cmdLS import ls
 from CMDP.cmdMKDIR import mkDir
 from CMDP.cmdMV import mv
 from CMDP.cmdPWD import pwd
-from CMDP.cmdRM import rm
+from CMDP.cmdRM import rm, rm_RF, rmDir
 from CMDP.cmdTAIL import tail
 from CMDP.cmdWC import wc
